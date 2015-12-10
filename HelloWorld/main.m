d3c75cc3db00a7f49d76226805cdfc3ea8d0add9
Carlos Castro Brenes <ccasbre27@hotmail.com>
2015-12-09 22:31:11 -0600

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by user on 12/9/15.
+//  Copyright © 2015 user. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
